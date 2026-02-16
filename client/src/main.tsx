88a590deefeb88e87ee9de7fb4f795d94099f1f8
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-16 17:18:46 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <div style={{ fontSize: '3rem', textAlign: 'center', padding: '100px' }}>
-    Hello from Render! Работает 💪
-  </div>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
