21d6d9a012ce6684e16ce75889f7da47423d7b6c
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-16 18:39:22 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +13,7 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
