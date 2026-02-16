3bd07512fd6dc68646f57e71961da603e2cdb55d
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-16 18:47:34 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,10 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { LanguageProvider } from "./context/LanguageContext";
 import { LanguageSwitcher } from "./components/LanguageSwitcher";
-import { Home } from "./pages/Home";
-import { Impressum } from "./pages/Impressum";
-import { Datenschutz } from "./pages/Datenschutz";
-import { NotFound } from "./pages/not-found";
+import Home from "./pages/Home";
+import Impressum from "./pages/Impressum";
+import Datenschutz from "./pages/Datenschutz";
+import NotFound from "./pages/not-found";
 
 function Router() {
   return (
