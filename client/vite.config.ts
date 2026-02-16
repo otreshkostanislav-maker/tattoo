1999d268bd664318067a9f14bc136602a201a65c
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-16 18:32:50 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Убедись, что это есть
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   root: './',
   publicDir: 'public',
   base: '/',
-  
-  // --- ДОБАВЬ ЭТОТ БЛОК ---
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // ------------------------
-
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'src/main.tsx'), // Проверь, правильно ли указан путь к main
+        main: path.resolve(__dirname, 'src/main.tsx'),
       },
     },
   },
 });
-
