28a9ba29a5b9939c137018b1faeea1bceb5a944d
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-16 16:37:35 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
-  return <div style={{ fontSize: '3rem', textAlign: 'center', padding: '50px' }}>
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
+  <div style={{ fontSize: '3rem', textAlign: 'center', padding: '100px' }}>
     Hello from Render! Работает 💪
   </div>
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+)
