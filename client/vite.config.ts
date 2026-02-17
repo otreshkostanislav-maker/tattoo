742785d8cfc25031e06f45195156a236db86b44d
otreshkostanislav-maker <otreshkostanislav@gmail.com>
2026-02-17 14:50:08 +0200

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- ДОБАВЬ ЭТО
+    tailwindcss(),
   ],
   base: '/',
   resolve: {
